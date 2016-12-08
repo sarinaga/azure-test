23aff783922ac1a37676b5e59faaa2b51087addb
TAKAHATA Tetsuya <t.takahata@nextware.co.jp>
2016-12-08 13:56:22 +0900

    php_db
    azure web apiからazure sql serverへの接続実験

---
+++ php_db/list.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<h1>登録済ユーザー一覧</h1>
+<?php
+  include './list_prog.php';
+  $stmt = getUserList();
+  print_r($stmt);
+?>
+</body>
+</html>
+
+
---
+++ php_db/list_prog.php
@@ -0,0 +1,33 @@
+<?php
+
+
+/**
+ * ユーザーリスト一覧取得
+ */
+function getUserList{
+
+	// Azure SQL server接続
+	try {
+	    $conn = new PDO("sqlsrv:server = tcp:takahata.database.windows.net,1433; Database = takahata", "takahata", "Aaaa1euy");
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e) {
+	    header('Content-Type: text/plain; charset=UTF-8', true, 500);	
+	    print("Error connecting to SQL Server.");
+    	die(print_r($e));
+	}
+
+	// userテーブル全取得
+	try {
+		$stmt = $pdo->query('SELECT * FROM users');
+	} catch (PDOException $e) {
+	    header('Content-Type: text/plain; charset=UTF-8', true, 500);	
+	    print("Error database access.");
+    	die(print_r($e));
+	}
+
+	return $stmt;
+
+}
+
+
