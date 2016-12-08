076c860f72b6a7a1a5f253d69b3b27cdca963bf2
TAKAHATA Tetsuya <t.takahata@nextware.co.jp>
2016-12-08 14:06:43 +0900

    list.php
    エラーが起きている場所をコメントアウト

--- php_db/list.php
+++ php_db/list.php
@@ -6,9 +6,9 @@
 <body>
 <h1>登録済ユーザー一覧</h1>
 <?php
-  include './list_prog.php';
-  $stmt = getUserList();
-  print_r($stmt);
+  //include './list_prog.php';
+  //$stmt = getUserList();
+  //print_r($stmt);
 ?>
 </body>
 </html>
