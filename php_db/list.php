d492291f22a33e026014de41fc03dc098b83a3ab
TAKAHATA Tetsuya <t.takahata@nextware.co.jp>
2016-12-13 16:36:32 +0900

    list.php
    列が表示されるはず

--- php_db/list.php
+++ php_db/list.php
@@ -11,8 +11,13 @@
 <body>
 <h1>登録済ユーザー一覧</h1>
 <?php
-  print_r($stmt);
-  print "<p>data base data</p>\n";
+  
+  
+  foreach ($stmt as $row) {
+    echo $row['id'].'：'.$row['name'];
+    echo "<br>\n";
+  
+  }
 ?>
 <p><a href="./">トップに戻る</a></p>
 
